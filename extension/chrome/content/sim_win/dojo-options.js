70e2bbe75a2e30879e5abd4fd68781657a516302
Steve Lee <steve@opendirective.com>
2008-08-14 16:33:47 +0000

    more reorg
    git-svn-id: http://subversion.assembla.com/svn/SIM_WIN/trunk@22 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ extension/chrome/content/sim_win/dojo-options.js
@@ -0,0 +1,9 @@
+// Options used in all dojo pages
+
+djConfig={                                     // see bootstrap.js
+        parseOnLoad: true, 
+        //isDebug: true,                                // enable Firebug lite if firebug not installed
+        //usePlainJson: true,                         // avoids warning from Ajax Hijack protection
+        //modulePaths: {sim_win: "../../../digits"}, // relative to dojo.js
+        //baseUrl : 'file:///H:/sim_win/js/dojo-release-1.1.1/dojo/'
+        };
