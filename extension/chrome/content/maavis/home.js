931cec11e3b452a94ace0cd115108adb763ef3fa
Steve Lee <steve@opendirective.com>
2009-09-05 11:55:36 +0000

    Fixed old home page
    git-svn-id: http://subversion.assembla.com/svn/SIM_WIN/trunk@112 e0e46c49-be69-4f5a-ad62-21024a331aea

--- extension/chrome/content/maavis/home.js
+++ extension/chrome/content/maavis/home.js
@@ -2,8 +2,11 @@ function loadPage()
 {
     window.removeEventListener('load', loadPage, false);
 	
-    const pad = mainwindow.getElementById("pad");
-    page.addFolderKeys(pad, "file:///%User%", true, null);
+    if (page.config.userType == 'scan') // TODO temp so old screens still work
+    {
+        const pad = mainwindow.getElementById("pad");
+        page.addFolderKeys(pad, "file:///%User%", true, null);
+    }
 }
 
 window.addEventListener('load', loadPage, false);
