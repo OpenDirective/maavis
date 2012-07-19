29e1dc6e9dc68d02f2c93f247b8986c6d479e6b5
Steve Lee <steve@opendirective.com>
2012-07-19 15:58:31 +0000

    Minor - removed extra lines
    git-svn-id: http://subversion.assembla.com/svn/SIM_WIN/trunk@269 e0e46c49-be69-4f5a-ad62-21024a331aea

--- DevMaavisPortable.cmd
+++ DevMaavisPortable.cmd
@@ -4,7 +4,6 @@ rem also uses the source copy of the default media set - so edit with care
 rem You will need to install FirefoxPortable to MaavisPortable\App
 
 set MaavisExtension=%CD%\extension
-set FirefoxPortableExe=%CD%\installer\3rdParty\FirefoxPortable\FirefoxPortable.exe
 rem start "Maavis" /b installer\MaavisPortable\MaavisPortable.exe -user ethel -jsconsole -nokiosk -quickstart -mediafolder "%CD%\media"
 start "Maavis" /b installer\MaavisPortable\MaavisPortable.exe -jsconsole -nokiosk -quickstart -mediafolder "%CD%\media"
 
